df62714019822e68c6800d80f7b89cd556730031
Hossein Naseri <hossein19naseri99@gmail.com>
2022-04-09 11:13:51 +0430

    cors2

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const appRootPath = require("app-root-path");
 const express = require("express");
 const articlesRoutes = require("./src/routes/app/articles");
+var cors = require("cors");
 const adminRoutes = require("./src/routes/admin/auth");
 const app = express();
 
 require("./src/config/env")();
 require("./src/config/db/DB_config")();
+app.use(cors());
 app.use("/static", express.static(appRootPath + "/src/public"));
 app.use(express.json());
 app.get("/", (req, res) => res.send("Hello World!"));
