0f4fca1c4e20a6ee183cffa07c24c6cde5dbedaf
Hossein Naseri <hossein19naseri99@gmail.com>
2022-05-18 02:50:24 -0600

    fix add new Agancy

--- src/routes/app/agency/index.js
+++ src/routes/app/agency/index.js
@@ -1,5 +1,8 @@
 const { Router } = require("express");
+const sharp = require("sharp");
 const Agancy = require("../../../model/agancy/Agancy");
+const { checkImage } = require("../../../utils/checkImage");
+const { getPath } = require("../../../utils/getPath");
 
 const app = Router();
 
@@ -20,6 +23,24 @@ app.get("/", async (req, res) => {
 app.post("/add", async (req, res) => {
   try {
     const { title, rate } = req.body;
+
+    if (!req.files?.image)
+      return res.status(400).json({
+        errors: ["عکس شناسه را فراموش کردید !"],
+      });
+    const picture = req.files?.image;
+    if (checkImage(picture)) {
+      await sharp(picture.data)
+        .jpeg({ quality: 60 })
+        .toFile(
+          getPath(`public/uploads/${picture.md5 + picture.name}.jpg`)
+        )
+        .catch((err) => console.log(err));
+    } else {
+      return res.status(400).json({
+        errors: ["عکس با فرمت png یا jpg  آپلود شود !"],
+      });
+    }
     await Agancy.create({ title, rate, picture });
     res.status(200).json({ message: "آژانس جدید ثبت شد !" });
   } catch (ex) {
